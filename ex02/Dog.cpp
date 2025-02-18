4fd3c39c70704097e494d2a2147f4b84bdd46e54
kurrrru <kurrrru8850@gmail.com>
2025-02-19 01:19:27 +0900

    update

--- ex02/Dog.cpp
+++ ex02/Dog.cpp
@@ -4,16 +4,16 @@
 #include <string>
 
 namespace {
-    namespace color {
-        const std::string red = "\033[1;31m";
-        const std::string green = "\033[1;32m";
-        const std::string yellow = "\033[1;33m";
-        const std::string blue = "\033[1;34m";
-        const std::string magenta = "\033[1;35m";
-        const std::string cyan = "\033[1;36m";
-        const std::string reset = "\033[0m";
-    }
+namespace color {
+const char red[] = "\033[1;31m";
+const char green[] = "\033[1;32m";
+const char yellow[] = "\033[1;33m";
+const char blue[] = "\033[1;34m";
+const char magenta[] = "\033[1;35m";
+const char cyan[] = "\033[1;36m";
+const char reset[] = "\033[0m";
 }
+}  // namespace
 
 Dog::Dog() : Animal("Dog") {
     std::cout << "Dog " << color::cyan << _type
